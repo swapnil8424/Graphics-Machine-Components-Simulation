7aceb552c3c92e35a41ab8a3127d4b3fdb67b490
Ram-Aditya <ramaditya.danbrown@gmail.com>
2018-04-12 22:15:14 +0530

    Cleaned master

--- MyProject/header.h
+++
@@ -1,3 +0,0 @@
-float **Rotation(float X, float Y,double angle,float *x,float *y);
-float **TranslateMatrix(float X,float Y,float *x, float *y);
-float **ScalingMatrix(float Sx,float Sy,float *x,float *y);
