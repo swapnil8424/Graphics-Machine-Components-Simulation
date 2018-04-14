923e8f37ed8b6f7441fcf1a8ce0de56bfb80a3d5
Manask322 <32352247+Manask322@users.noreply.github.com>
2018-04-14 23:31:31 +0530

    Update Piston and integrated Code

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -16,18 +16,17 @@ float vert_inx[]={130,130,270,270};
 float vert_iny[]={310,700,700,310};
 float circle[]={200,380};
 float trans_point[]={circle[0]-20,circle[1],circle[0]+20,circle[1]};
-float circle_rot[]={275,210};
-float rot_point[]={circle_rot[0]-55,circle_rot[1],circle_rot[0]+55,circle_rot[1]};
+float circle_rot[]={200,125};
 float **ax4 ;
 float X, Y, X1, Y1;
-float count1 = 0,y_coordinate ,translate = 10 ;
-double angle = 5*3.14/180 ;
+float count1 = 0,y_coordinate ,translate = 13.33 ;
+double angle = 9*3.14/180 ;
 double angle1 = 0 ;
 void myInit() {
 glClear(GL_COLOR_BUFFER_BIT);
 glClearColor(1.0, 0.9, 0.8, 1.0);
 glMatrixMode(GL_PROJECTION);
-gluOrtho2D(0, 640, 0, 800);
+gluOrtho2D(-500, 1500,-200, 1000);
 }
 
 void draw_pixel(int x, int y) {
@@ -38,20 +37,17 @@ glEnd();
 }
 void drawcircle(float a,float b,float c)
 {
-    if(c==150)
-    {
+    if(c==150){
         printf("Here!\n");
     }
     draw_pixel(a,b);
     float x=0;
     float y=c;
     float h=1-c;
-    while(y>x)
-    {
+    while(y>x){
         if(h<0)
             h=h+(2*x)+3;
-        else
-        {
+        else{
             h=h+(2*(x-y))+5;
             y--;
         }
@@ -73,73 +69,131 @@ void draw_line(float x1, float x2, float y1, float y2) {
     float x,y;
     dx = x2-x1;
     dy = y2-y1;
-    if (dx < 0) dx = -dx;
-    if (dy < 0) dy = -dy;
+    if (dx < 0)
+        dx = -dx;
+    if (dy < 0)
+        dy = -dy;
     incx = 1;
-    if (x2 < x1) incx = -1;
+    if (x2 < x1)
+        incx = -1;
     incy = 1;
-    if (y2 < y1) incy = -1;
-    x = x1; y = y1;
+    if (y2 < y1)
+        incy = -1;
+    x = x1;
+    y = y1;
     if (dx > dy) {
-    draw_pixel(x, y);
-    e = 2 * dy-dx;
-    inc1 = 2*(dy-dx);
-    inc2 = 2*dy;
-    for (i=0; i<dx; i++) {
-    if (e >= 0) {
-    y += incy;
-    e += inc1;
+        draw_pixel(x, y);
+        e = 2 * dy-dx;
+        inc1 = 2*(dy-dx);
+        inc2 = 2*dy;
+        for (i=0; i<dx; i++) {
+            if (e >= 0) {
+                y += incy;
+                e += inc1;
+            }
+            else
+                e += inc2;
+            x += incx;
+            draw_pixel(x, y);
+        }
     }
-    else
-    e += inc2;
-    x += incx;
-    draw_pixel(x, y);
-    }
-    } else {
-    draw_pixel(x, y);
-    e = 2*dx-dy;
-    inc1 = 2*(dx-dy);
-    inc2 = 2*dx;
-    for (i=0; i<dy; i++) {
-    if (e >= 0) {
-    x += incx;
-    e += inc1;
-    }
-    else
-    e += inc2;
-    y += incy;
-    draw_pixel(x, y);
-    }
+    else {
+        draw_pixel(x, y);
+        e = 2*dx-dy;
+        inc1 = 2*(dx-dy);
+        inc2 = 2*dx;
+        for (i=0; i<dy; i++){
+            if (e >= 0) {
+                x += incx;
+                e += inc1;
+            }
+            else
+                e += inc2;
+                y += incy;
+                draw_pixel(x, y);
+        }
     }
     count1++ ;
-//    cout << count1 << endl ;
-//glFlush();
+
 }
+void Piston(){
 
-void myDisplay() {
-
-
-//draw_line(x1, x2, y1, y2);
-//draw_line(x2, x3, y2, y3);
-//draw_line(x3, x4, y3, y4);
-//draw_line(x4, x1, y4, y1);
-        for(int i=0;i<11;i++)
-    {
+    //Drawing Piston
+    for(int i=0;i<11;i++){
             draw_line(vert_x[i],vert_x[i+1],vert_y[i],vert_y[i+1]);
     }
     draw_line(vert_x[11],vert_x[0],vert_y[11],vert_y[0]) ;
-    //drawcircle(275,210,55);
+
+    //Drawing upper circles (Two Circle)
+    drawcircle(circle[0],circle[1],15);
+    drawcircle(circle[0],circle[1],20);
+
+    //Drawing upper translating circle
+    drawcircle(circle_rot[0],circle_rot[1],55);
+
+    //Drawing crankshaft
+    draw_line(trans_point[0],circle_rot[0]-55,trans_point[1],circle_rot[1]);
+    draw_line(trans_point[2],circle_rot[0]+55,trans_point[3],circle_rot[1]);
+
+
+
+    //Translating condition
+    long long int ab = 1000000;
+    while(ab--){;}
+    if( vert_y[11] >= 700 ){
+        translate = -10 ;
+    }
+    else if( vert_y[0] <= 500 ){
+        translate = 10 ;
+    }
+
+    //Translating Piston
+    for(int i = 0 ; i < 12 ; i++){
+        ax4 = TranslateMatrix(0,translate,&vert_x[i],&vert_y[i]) ;
+        cout << "a[0]" << ax4[0][0] << " " << ax4[1][0] << endl ;
+        vert_x[i] = ax4[0][0] ;
+        vert_y[i] = ax4[1][0] ;
+    }
+
+    //Translating upper circle
+    ax4 = TranslateMatrix(0,translate,&circle[0],&circle[1]) ;
+    circle[0]=ax4[0][0];
+    circle[1]=ax4[1][0];
+
+    //Rotating lower circle
+    ax4 = Rotation(200,200,angle,&circle_rot[0],&circle_rot[1]);
+    circle_rot[0]=ax4[0][0];
+    circle_rot[1]=ax4[1][0];
+
+    //Translating upper points
+    ax4 = TranslateMatrix(0,translate,&trans_point[0],&trans_point[1]);
+    trans_point[0]=ax4[0][0];
+    trans_point[1]=ax4[1][0];
+    ax4 = TranslateMatrix(0,translate,&trans_point[2],&trans_point[3]);
+    trans_point[2]=ax4[0][0];
+    trans_point[3]=ax4[1][0];
+
+    //Rotating Lower points
+//    ax4 = Rotation(200,200,angle,&rot_point[0],&rot_point[1]);
+//    rot_point[0]=ax4[0][0];
+//    rot_point[1]=ax4[1][0];
+//    ax4 = Rotation(200,200,angle,&rot_point[2],&rot_point[3]);
+//    rot_point[2]=ax4[0][0];
+//    rot_point[3]=ax4[1][0];
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+}
+void myDisplay() {
+
+    //Drawing lower fixed Circles
     drawcircle(200,200,30);
     drawcircle(200,200,40);
     drawcircle(200,200,150);
     drawcircle(200,200,130 );
-    drawcircle(circle[0],circle[1],15);
-    drawcircle(circle[0],circle[1],20);
-    drawcircle(circle_rot[0],circle_rot[1],55);
-    draw_line(trans_point[0],rot_point[0],trans_point[1],rot_point[1]);
-    draw_line(trans_point[2],rot_point[2],trans_point[3],rot_point[3]);
+
+    //Drawing the outer case
     cout << vert_y[0] << " " << vert_y[11] << " " << y_coordinate << endl ;
-    glClear(GL_COLOR_BUFFER_BIT);
      for(int i=0;i<3;i++)
     {
             draw_line(vert_outx[i],vert_outx[i+1],vert_outy[i],vert_outy[i+1]);
@@ -149,114 +203,26 @@ void myDisplay() {
             draw_line(vert_inx[i],vert_inx[i+1],vert_iny[i],vert_iny[i+1]);
     }
 
-    draw_line(trans_point[0],rot_point[0],trans_point[1],rot_point[1]);
-    draw_line(trans_point[2],rot_point[2],trans_point[3],rot_point[3]);
-    long long int ab = 100000000;
-    while(ab--){;}
-    if( vert_y[11] >= 700 ){
-        translate = -10 ;
-    }
-    else if( vert_y[0] <= 460 ){
-        translate = 10 ;
-    }
+    draw_line(trans_point[0],circle_rot[0]-55,trans_point[1],circle_rot[1]);
+    draw_line(trans_point[2],circle_rot[0]+55,trans_point[3],circle_rot[1]);
     cout << "here" << endl ;
-    for(int i = 0 ; i < 12 ; i++){
 
-        ax4 = TranslateMatrix(0,translate,&vert_x[i],&vert_y[i]) ;
-        cout << "a[0]" << ax4[0][0] << " " << ax4[1][0] << endl ;
-        vert_x[i] = ax4[0][0] ;
-        vert_y[i] = ax4[1][0] ;
-    }
-    ax4 = TranslateMatrix(0,translate,&circle[0],&circle[1]) ;
-    circle[0]=ax4[0][0];
-    circle[1]=ax4[1][0];
-    ax4 = TranslateMatrix(0,translate,&trans_point[0],&trans_point[1]);
-    trans_point[0]=ax4[0][0];
-    trans_point[1]=ax4[1][0];
-    ax4 = TranslateMatrix(0,translate,&trans_point[2],&trans_point[3]);
-    trans_point[2]=ax4[0][0];
-    trans_point[3]=ax4[1][0];
-    ax4 = Rotation(200,200,1,&circle_rot[0],&circle_rot[1]);
-    circle_rot[0]=ax4[0][0];
-    circle_rot[1]=ax4[1][0];
-    ax4 = Rotation(200,200,1,&rot_point[0],&rot_point[1]);
-    rot_point[0]=ax4[0][0];
-    rot_point[1]=ax4[1][0];
-    ax4 = Rotation(200,200,1,&rot_point[2],&rot_point[3]);
-    rot_point[2]=ax4[0][0];
-    rot_point[3]=ax4[1][0];
+//    glutSwapBuffers();
+    Piston();
 
-
-
-//    ax = TranslateMatrix(0,translate,&x2,&y2) ;
-//    angle1 += angle ;
-//    x2 = ax[0][0] ;
-//    y2 = ax[1][0] ;
-//    float **ax1 ;
-//    ax1 = TranslateMatrix(0,translate,&x3,&y3) ;
-//    angle1 += angle ;
-//    x3 = ax1[0][0] ;
-//    y3 = ax1[1][0] ;
-//    float **ax2 ;
-//    ax2 = TranslateMatrix(0,translate,&x4,&y4) ;
-//    angle1 += angle ;
-//    x4 = ax2[0][0] ;
-//    y4 = ax2[1][0] ;
-    y_coordinate += 10 ;
 
 }
 int main(int argc, char **argv) {
-printf( "Enter (x1, y1, x2, y2)\n");
-x1 = 200; y1 = 20 ; x2 = 300 ; y2 = 20 ; x3 = 300;y3 =100;x4 = 200; y4 =100 ;
-X =x1 ;
-Y =y1 ;
-X1 = x2 ;
-Y1= y2 ;
-y_coordinate = x1;
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-glutInitWindowSize(500, 500);
-glutInitWindowPosition(0, 0);
-glutCreateWindow("Bresenham's Line Drawing");
-myInit();
-glutDisplayFunc(myDisplay);
-glutIdleFunc(myDisplay) ;
-glutMainLoop();
-return(0);
+    printf( "Enter (x1, y1, x2, y2)\n");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(1470, 720);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Bresenham's Line Drawing");
+    myInit();
+    glutDisplayFunc(myDisplay);
+    glutIdleFunc(myDisplay) ;
+    glutMainLoop();
+    return(0);
 }
-//#include <windows.h>
-//#include <GL/glut.h>
-//#include <stdlib.h>
-//#include <math.h>
-//#include<iostream>
-//#include "header.h"
-//using namespace std;
-//
-//void reshape(int w, int h)
-//{
-//glViewport(0.0, 0.0, w, h);
-//}
-//void intiopenGL()
-//{
-//glClearColor(1.0, 1.0, 1.0, 1.0);
-//}
-//int main(int argc, char** argv)
-//{
-//cout<<"enter centre point \n";
-//    cin>>x0>>y00 ;
-//cout<<"enter semi-minor and semi-major axis\n";
-//cin>>b>>a;
-//glutInit(&argc, argv);
-//glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA );
-//glutInitWindowSize(800, 600);
-//glutInitWindowPosition(0,0);
-//glutCreateWindow("Ellipse algo");
-//gluOrtho2D(-400,400,-300,300);//x-axis:-400-400 ; y-axis: -300-300
-//intiopenGL();
-//
-//glutDisplayFunc(rotate_ellipse);
-//glutIdleFunc(rotate_ellipse);
-//glutReshapeFunc(reshape);
-//glutMainLoop();
-//return 0;
-//}
+
